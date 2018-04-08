c9568453b17338e42c8175ede23b77e21ee82ec9
zhanghang1995 <749931552@qq.com>
2018-04-08 12:04:26 +0800

    2018/4/8号更改

--- app/src/main/java/com/example/a74993/speaktest02/utils/time/MyTimerTask.java
+++ app/src/main/java/com/example/a74993/speaktest02/utils/time/MyTimerTask.java
@@ -29,27 +29,27 @@ public class MyTimerTask extends TimerTask {
         /**
          * 此处通过name的不同分别执行不同的Task任务
          */
-        if (name.equals("起床")&&(TimeUtils.getSystemClockUpper().toString()).equals("07:00")) {
+        if (name.equals("起床")&&(TimeUtils.getSystemClockUpper().toString()).equals("11:59")) {
             SpeechUpload.upload("起床", context_this, 0);
             LogInfo.e("当前系统时间", (TimeUtils.getSystemClockUpper().toString().trim()));
             LogInfo.e("Thread", "起床服务");
-        }else if((TimeUtils.getSystemClockUpper().toString()).equals("07:30")){  //早饭服务
+        }else if((TimeUtils.getSystemClockUpper().toString()).equals("12:00")){  //早饭服务
             SpeechUpload.upload("早饭服务", context_this, 1);
             LogInfo.e("当前系统时间", (TimeUtils.getSystemClockUpper().toString().trim()));
             LogInfo.e("Thread", "早饭服务");
-        }else if((TimeUtils.getSystemClockUpper().toString()).equals("11:30")){   //日程安排
+        }else if((TimeUtils.getSystemClockUpper().toString()).equals("12:01")){   //日程安排
             SpeechUpload.upload("日程安排", context_this, 2);
             LogInfo.e("当前系统时间", (TimeUtils.getSystemClockUpper().toString().trim()));
             LogInfo.e("Thread", "日程安排服务");
-        }else if((TimeUtils.getSystemClockUpper().toString()).equals("11:42")){   //午饭服务
+        }else if((TimeUtils.getSystemClockUpper().toString()).equals("12:02")){   //午饭服务
             SpeechUpload.upload("午饭服务", context_this, 3);
             LogInfo.e("当前系统时间", (TimeUtils.getSystemClockUpper().toString().trim()));
             LogInfo.e("Thread", "午饭服务");
-        }else if((TimeUtils.getSystemClockUpper().toString()).equals("1:00")){   //午觉提醒
+        }else if((TimeUtils.getSystemClockUpper().toString()).equals("12:03")){   //午觉提醒
             SpeechUpload.upload("午觉提醒", context_this, 4);
             LogInfo.e("当前系统时间", (TimeUtils.getSystemClockUpper().toString().trim()));
             LogInfo.e("Thread", "午觉提醒服务");
-        }else if((TimeUtils.getSystemClockUpper().toString()).equals("17:33")){   //晚饭服务
+        }else if((TimeUtils.getSystemClockUpper().toString()).equals("11:53")){   //晚饭服务
             SpeechUpload.upload("晚饭服务", context_this, 5);
             LogInfo.e("当前系统时间", (TimeUtils.getSystemClockUpper().toString().trim()));
             LogInfo.e("Thread", "晚饭服务");
